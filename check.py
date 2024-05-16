5a3b9501c92ae1ba991fd19b22ccb8ddd259066d
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-05-16 03:26:04 +0300

    adding check and tests folder

---
+++ check.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Welcome to our AirBnB clone console project
+"""
+
+def welcome_message():
+    """prints welcome message to check style"""
+    print("Welcome to our AirBnB clone console project")
+
+if __name__ == "__main__":
+    welcome_message()
