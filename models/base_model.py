61cb900a5ed21b12a4d0ffae255a138c9183c6f7
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-05-16 07:58:52 +0300

    first test

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
-"""Base Model file"""
+"""Base Model for AirBnB project console"""
 from datetime import datetime
 import models
 import uuid
 
 
 class BaseModel:
-    """BaseModel class instances and methods"""
+    """BaseModel class instances with methods"""
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.updated_at = datetime.now()
+        self.created_at = datetime.now()
         if kwargs:
             dictionary_mod = self.__dict__
-            for key, value in kwargs.items():
-                if ((key == "created_at") or (key == "updated_at")):
-                    time_form = datetime.strptime(kwargs[key],
+            for item, result in kwargs.items():
+                if item == "created_at" or item == "updated_at":
+                    dictionary_mod[item] = datetime.strptime(kwargs[item],
                                                   "%Y-%m-%dT%H:%M:%S.%f")
-                    dictionary_mod[key] = time_form
-                elif (key != "__class__"):
-                    dictionary_mod[key] = value
+                else:
+                    dictionary_mod[item] = result
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
+
+    def to_dict(self):
+        """returns dict contains variables and class name"""
+        instance_dict = self.__dict__.copy()
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        instance_dict["__class__"] = self.__class__.__name__
+        return instance_dict
+
+    def save(self):
+        """Save the file"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def __str__(self):
         """returns string defining class"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, dict(self.__dict__))
+                                     self.id,
+                                     dict(self.__dict__))
 
-    def save(self):
-        """Saves file"""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """returns dict containing variables and class name"""
-        instance_dict = self.__dict__.copy()
-        instance_dict["__class__"] = self.__class__.__name__
-        instance_dict["created_at"] = self.created_at.isoformat()
-        instance_dict["updated_at"] = self.updated_at.isoformat()
-        return instance_dict
