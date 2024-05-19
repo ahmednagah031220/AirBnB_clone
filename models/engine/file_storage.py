5387b3765991d58b33ccc87e1c2d4eea64843b8b
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-05-19 08:09:55 +0300

    update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,6 @@ import os
 
 
 class FileStorage:
-
     """Class for storing and retrieving data"""
     __file_path = "file.json"
     __objects = {}
@@ -21,7 +20,7 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
+        """serializes __objects to the JSON file (path: __file_path)"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
@@ -36,13 +35,15 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+        }
         return classes
 
     def reload(self):
@@ -51,45 +52,50 @@ class FileStorage:
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
+            obj_dict = {
+                k: self.classes()[v["__class__"]](**v)
+                for k, v in obj_dict.items()
+            }
             # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
         """Returns the valid attributes and their types for classname"""
         attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime,
+            },
+            "User": {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str,
+            },
+            "State": {"name": str},
+            "City": {
+                "state_id": str,
+                "name": str,
+            },
+            "Amenity": {"name": str},
+            "Place": {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list,
+            },
+            "Review": {
+                "place_id": str,
+                "user_id": str,
+                "text": str,
+            },
         }
         return attributes
